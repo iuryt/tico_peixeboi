4ce90dda2606c57acbb764e71de1e52c7f8f4135
iury simoes-sousa <simoesiury@gmail.com>
2022-11-30 16:03:58 -0500

    fix x coordinate and set time coordinates with hours

--- src/trajectory.py
+++ src/trajectory.py
@@ -2,10 +2,19 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from dateparser import parse
 
-df = pd.read_csv("../data/planilhaSateliteTicoR4.csv", delimiter=";", parse_dates=["Data"], index_col="Data")
+# load data
+df = pd.read_csv("../data/planilhaSateliteTicoR4.csv", delimiter=";")
+
+# parse timestamps
+time = (df["Data"]+" "+df["Hora"]).apply(parse)
+
+# set index
 df = df[["Longitude","Latitude"]]
+df.index = time
 df.index.name = "time"
 
 ds = df.to_xarray()
-ds = ds.assign(x = 111.32e3 * np.cos(np.deg2rad(ds.Latitude)),y = 110.574e3 * ds.Latitude)
+ds = ds.assign(x = 111.32e3 * np.cos(np.deg2rad(ds.Latitude)) * ds.Longitude,y = 110.574e3 * ds.Latitude)
+ds = ds.assign(x = ds.x-ds.x.isel(time=0), y = ds.y-ds.y.isel(time=0))
